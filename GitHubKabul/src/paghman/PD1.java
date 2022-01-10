46b99580a31ed63c6dc35050101fc6e863f913ab
Shirinzoy <rahim.shirinzoy@gmail.com>
2022-01-10 17:37:23 -0500

    first commit

---
+++ GitHubKabul/src/paghman/PD1.java
@@ -0,0 +1,10 @@
+package paghman;
+
+public class PD1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ GitHubKabul/src/shakardara/PD1.java
@@ -0,0 +1,10 @@
+package shakardara;
+
+public class PD1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
